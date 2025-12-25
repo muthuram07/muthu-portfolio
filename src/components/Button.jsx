5837122f9be7ab8b0b8f6fff6c0ea2f3ad92b3ea
Muthuraman Saravanan <129589774+muthuram07@users.noreply.github.com>
2025-12-25 12:00:44 +0530

    Delete src directory

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import * as styles from './Button.css.ts';
-
-export default function Button({ children, href }) {
-  const Comp = href ? 'a' : 'button';
-  return <Comp className={styles.button} href={href}>{children}</Comp>;
-}
